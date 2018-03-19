fecc43ffe5a5242240e13e79c399e6f77df07e61
李正先 <hzlizx@tairanchina.com>
2018-03-19 16:39:42 +0800

    整合mybatis

--- src/main/java/com/lzx2005/lzx2005webapi/Lzx2005WebApiApplication.java
+++ src/main/java/com/lzx2005/lzx2005webapi/Lzx2005WebApiApplication.java
@@ -1,9 +1,11 @@
 package com.lzx2005.lzx2005webapi;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.lzx2005")
 public class Lzx2005WebApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/lzx2005/lzx2005webapi/controller/HelloController.java
+++ src/main/java/com/lzx2005/lzx2005webapi/controller/HelloController.java
@@ -1,7 +1,13 @@
 package com.lzx2005.lzx2005webapi.controller;
 
+import com.lzx2005.lzx2005webapi.dao.BlogMapper;
+import com.lzx2005.lzx2005webapi.model.Blog;
+import com.lzx2005.lzx2005webapi.model.BlogExample;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by Lizhengxian on 2018/3/19.
@@ -9,8 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private BlogMapper blogMapper;
+
     @GetMapping("/")
     public String hello(){
+
+        BlogExample blogExample = new BlogExample();
+        List<Blog> blogs = blogMapper.selectByExample(blogExample);
+        blogs.forEach(blog -> {
+            System.out.println(blog.getTitle());
+        });
         return "hello";
     }
 }
